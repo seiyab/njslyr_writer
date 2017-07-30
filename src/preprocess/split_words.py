245719a53c2080e178f1bf8dc8dc528b1e8aa15d
seiya <hoge@hoge>
2017-07-30 18:22:11 +0900

    typo

--- src/preprocess/split_words.py
+++ src/preprocess/split_words.py
@@ -34,6 +34,6 @@ def main():
 
 def parse(sentence):
     words = m.parse(sentence).split("\n")[:-1]
-    return map(lambda x: x.split("\t")[0], tokens)
+    return map(lambda x: x.split("\t")[0], words)
 
 main()
