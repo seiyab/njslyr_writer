03413558f1f6582656cbd448db9689498c62420b
seiya <hoge@hoge>
2017-07-30 22:08:24 +0900

    write.py

--- src/generate/write.py
+++ src/generate/write.py
@@ -1,34 +1,31 @@
 import warnings
 warnings.simplefilter('ignore', DeprecationWarning)
-import sys
 import pickle
 import json
 from argparse import ArgumentParser
-from os import listdir
-from collections import Counter
 import numpy as np
 from rnn import RNN
 
+preprocessed_dir = Path('/preprocessed')
+models_dir = Path('/models')
+
 def pick_head():
-    head = None
-    while head is None:
-        try:
-            with open('/home/ninja/translated/' + np.random.choice(listdir('/home/ninja/translated')), 'r') as f:
-                s = json.load(f)
-                head = np.random.choice(s)[0]
-        except ValueError:
-            continue
+    with open(np.random.choice(list(preprocessed_dir.glob('tokens/*.json'))), 'r') as f:
+        tweets = json.load(f)
+        head = np.random.choice(tweets)[0]
     return head
 
-def write(model, head=None):
-    with open('/home/ninja/translated/dicts/words.json', 'r') as f:
-        words = json.load(f)
-    word2ind = {w: i for i, w in enumerate(words)}
+def write(generator, head=None):
+    with open(preprocessed_dir / 'dictionaries' / 'word2idx.json', 'r') as f:
+        word2idx = json.load(f)
+    words = [-1] * len(word2idx)
+    for w, i in word2idx:
+        words[i] = w
     if head is None:
         head = pick_head()
     else:
         head = word2ind[head]
-    with open(model, 'rb') as f:
+    with open(generator, 'rb') as f:
         lstm = pickle.load(f)
     generated = "".join([words[i] for i in lstm.sample(head, word2ind['EOS'])])
     return generated.rstrip('EOS')
