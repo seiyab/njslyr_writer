4f63720ecafd784ee6a9b8848ef899ee545267b7
seiya <hoge@hoge>
2017-07-30 21:10:00 +0900

    typo

--- src/preprocess/count_words.py
+++ src/preprocess/count_words.py
@@ -6,7 +6,7 @@ preprocessed_dir = Path('/preprocessed')
 
 def main():
     c = Counter()
-    for jsonfile in preprocessed_dir.glob('*.json'):
+    for jsonfile in preprocessed_dir.glob('tokens/*.json'):
         with open(jsonfile, 'r') as f:
             c.update(sum(json.load(f), []))
     with open(preprocessed_dir / 'dictionaries' / 'bag_of_words.json', 'w') as f:
